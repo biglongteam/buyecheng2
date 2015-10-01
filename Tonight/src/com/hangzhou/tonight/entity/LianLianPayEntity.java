a19b437cb544bf846619030ecd8a1e35a390c302
zhangyanchao <yanchao1011@126.com>
2015-10-01 18:14:43 +0800

    1,qq登录2 ，连连支付集成

---
+++ Tonight/src/com/hangzhou/tonight/entity/LianLianPayEntity.java
@@ -0,0 +1,102 @@
+package com.hangzhou.tonight.entity;
+
+public class LianLianPayEntity extends Entity {
+	
+	private String busi_partner;
+
+	private String dt_order;
+
+	private String info_order;
+
+	private String money_order;
+
+	private String name_goods;
+
+	private String no_order;
+
+	private String notify_url;
+
+	private String oid_partner;
+
+	private String risk_item;
+
+	private String sign;
+
+	private String sign_type;
+
+	private String valid_order;
+
+	public void setBusi_partner(String busi_partner){
+	this.busi_partner = busi_partner;
+	}
+	public String getBusi_partner(){
+	return this.busi_partner;
+	}
+	public void setDt_order(String dt_order){
+	this.dt_order = dt_order;
+	}
+	public String getDt_order(){
+	return this.dt_order;
+	}
+	public void setInfo_order(String info_order){
+	this.info_order = info_order;
+	}
+	public String getInfo_order(){
+	return this.info_order;
+	}
+	public void setMoney_order(String money_order){
+	this.money_order = money_order;
+	}
+	public String getMoney_order(){
+	return this.money_order;
+	}
+	public void setName_goods(String name_goods){
+	this.name_goods = name_goods;
+	}
+	public String getName_goods(){
+	return this.name_goods;
+	}
+	public void setNo_order(String no_order){
+	this.no_order = no_order;
+	}
+	public String getNo_order(){
+	return this.no_order;
+	}
+	public void setNotify_url(String notify_url){
+	this.notify_url = notify_url;
+	}
+	public String getNotify_url(){
+	return this.notify_url;
+	}
+	public void setOid_partner(String oid_partner){
+	this.oid_partner = oid_partner;
+	}
+	public String getOid_partner(){
+	return this.oid_partner;
+	}
+	public void setRisk_item(String risk_item){
+	this.risk_item = risk_item;
+	}
+	public String getRisk_item(){
+	return this.risk_item;
+	}
+	public void setSign(String sign){
+	this.sign = sign;
+	}
+	public String getSign(){
+	return this.sign;
+	}
+	public void setSign_type(String sign_type){
+	this.sign_type = sign_type;
+	}
+	public String getSign_type(){
+	return this.sign_type;
+	}
+	public void setValid_order(String valid_order){
+	this.valid_order = valid_order;
+	}
+	public String getValid_order(){
+	return this.valid_order;
+	}
+
+}
